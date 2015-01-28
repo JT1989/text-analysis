69b3ac3d90e3e75aad796f6249d05e5127fd601b
JT1989 <jtiao@kissmetrics.com>
2015-01-27 22:30:32 -0800

    Added v1.1 Basic Frequency Statistics

--- textalyze.rb
+++ textalyze.rb
@@ -55,16 +55,25 @@ end
 #content = File.read("sample_data/great-gatsby.txt")
 
 #[v1.0] Reading From a User-Supplied File
-if ARGV.size == 0
-  puts "Welcome! What is the filename that you want to anlayze?"
-  puts "For example, type: sample_data/great-gatsby.txt"
-  content = gets.chomp #removes the extra line
-else
-  content = ARGV[0]
+#if ARGV.size == 0
+#  puts "Welcome! What is the filename that you want to anlayze?"
+#  puts "For example, type: sample_data/great-gatsby.txt"
+#  content = gets.chomp #removes the extra line
+#else
+#  content = ARGV[0]
+#end
+
+#[v1.1] Basic Frequency Statistics
+def calculate_percentage(array) #calculates % of each item in an array
+  calculate_percentage = item_counts(array.map(&:downcase)) #{"a"=>3, "b"=>2, "ZZZ"=>1}
+
+  calculate_percentage.each do |key, value| # do |"a", 2|
+    calculate_percentage[key] = value.to_f/array.length.to_f #for "a" = 2.00/6.00
+  end
+
+  calculate_percentage #return "a" => 0.333 #returns a hash containing item and frequency
 end
 
-# "p" prints something to the screen in a way that's friendlier
-# for debugging purposes than print or puts.
 if __FILE__ == $0
 	#p item_counts([1,2,1,2,1]) == {1 => 3, 2 => 2}
 	#p item_counts(["a","b","a","b","a","ZZZ"]) == {"a" => 3, "b" => 2, "ZZZ" => 1}
@@ -73,18 +82,14 @@ if __FILE__ == $0
 	#p item_counts([true, nil, "dinosaur"]) == {true => 1, nil => 1, "dinosaur" => 1}
 	#p item_counts(["a","a","A","A"]) == {"a" => 2, "A" => 2}
 
-  #p string_to_characters("and") == {"a" => 1, "n" => 1, "d" => 1}
+  #p string_to_characters("Aand") == {"A" => 1, "a" => 1, "n" => 1, "d" => 1}
   #p string_to_characters("remember") == {"r" => 2, "e" => 3, "m" => 2, "b" => 1}
 
+  #p sanitize("Aand") == {"a" => 2, "n" => 1, "d" => 1}
   #p sanitize("ReMemBEr") == {"r" => 2, "e" => 3, "m" => 2, "b" => 1}
   #p sanitize("Dog iS slEepinG!") == {"d"=>1, "o"=>1, "g"=>2, " "=>2, "i"=>2, "s"=>2, "l"=>1, "e"=>2, "p"=>1, "n"=>1, "!"=>1}
 
-  p item_counts(sanitize(content))
+  #p item_counts(sanitize(content))
+
+  p calculate_percentage(["a","b","A","b","a","ZZZ"]) == {"a"=>0.5, "b"=>0.3333333333333333, "zzz"=>0.16666666666666666}
 end
-
-
-
-#   2. The code does what it should, but is conceptually confused
-#   3. Something else we haven't though of
-#
-# Remember: Option #3 is *always* possible.
